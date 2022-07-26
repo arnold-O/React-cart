33ec9464b22a4db90df0b1da830cb5c43f94c21b
arnold-oghiator <arnoldoghiator@gmail.com>
2022-07-26 19:30:34 +0100

    cart setup

---
+++ src/component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <div className='headerSection'>
+        <div className='mainSection'>
+        <div>
+        <p>UseReducer</p>
+        </div>
+        <div className='iconRight'>
+       
+            <p>cart</p>
+            <p className='position-abs'>0</p>
+         
+        </div>
+
+        </div>
+
+    </div>
+     <h1 className='cart-title'>Your Bag</h1>
+     </>
+  )
+}
